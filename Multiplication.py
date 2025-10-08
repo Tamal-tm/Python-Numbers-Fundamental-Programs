a30fb458a99f6b31411187803784d9c2c924ba04
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-08 20:06:16 +0530

    Add files via upload

---
+++ Multiplication.py
@@ -0,0 +1,12 @@
+num=int(input("Enter a number: "))
+
+for i in range(1, num):
+    print(num, " x ", i ," = " ,(num*i))
+
+num2=int(input("Enter a number: "))
+i=1
+while i<=10:
+    print(num2, " x ", i ," = " ,(num2*i))
+    i=i+1
+
+print("********************")
