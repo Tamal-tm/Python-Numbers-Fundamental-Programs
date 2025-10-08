1390f91baeed410e8ac09417f2ad06e4497b5c49
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-08 20:05:59 +0530

    Add files via upload

---
+++ Largest_3_nos.py
@@ -0,0 +1,15 @@
+num1=int(input("Enter 1st no."))
+num2=int(input("Enter 2nd no."))
+num3=int(input("Enter 3rd no."))
+
+if num1 > num2 and num2 > num3:
+     print("Largest number: ",num1)
+
+elif num2> num1 and num2> num1:
+     print("Largest number: ",num2)
+
+elif num3 > num1 and num3 > num2:
+     print("Largest number: ", num3)
+
+else: 
+     print('All are equal')
