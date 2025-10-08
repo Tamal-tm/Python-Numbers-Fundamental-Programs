e18f2b91401f64bf3eac58ff0197cd0ed1fa33cc
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-08 20:05:41 +0530

    Add files via upload

---
+++ HCF_GDC.py
@@ -0,0 +1,13 @@
+def findHCF(x,y):
+    if x>y:
+        smaller=y
+    else:
+        smaller=x
+    for i in range(1,smaller+1):
+        if x % i == 0 and y % i == 0:
+            hcf=i
+    return hcf
+
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+print("Hcf of given two numbers: ", findHCF(a,b))
