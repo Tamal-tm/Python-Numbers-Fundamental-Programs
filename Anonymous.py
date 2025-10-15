a3de3fde1fb5b76741b5900db177fb791535129a
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-15 08:49:28 +0530

    Update Anonymous.py

--- Anonymous.py
+++ Anonymous.py
@@ -5,7 +5,7 @@ nterms= int(input("Enter no. of terms: "))
 # 'map' Applies a function to every item in an iterable (range here).
 # lambda x: 2 ** x → An anonymous function that takes x and returns 2 raised to power x.
 # range(nterms+1) → Generates numbers from 0 up to 'nterms' inclusive.
-# list(...) → Converts the map object into a list of results.
+# list(...) → converts the map object into a list of results.
 
 result=list(map(lambda x: 2 ** x, range (nterms+1))) # map will let the function (lambda x) perform on all the iterables. 
 
@@ -13,6 +13,7 @@ print(result)
 
 for i in range(nterms+1):
     print("The value of 2 raised to the power", i, "is", result[i])
+
 
 
 
