c25899ce1fe37a61c37f5e7bb0ad0e4bf0b7b798
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-24 08:13:01 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,6 +2,7 @@
 a=0
 b=1
 num=int(input("Enter no. of terms: "))
+
 if num == 1:
     print(a)
     
@@ -29,6 +30,7 @@ else:
     print("Fibonacci series: ")
     for i in range(num):
         print(fibo(i))
+
 
 
 
