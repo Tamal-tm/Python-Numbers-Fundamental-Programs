00212f3aca9b367fd55c887ee5230ba0d56e49a3
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-26 15:55:03 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -15,7 +15,6 @@ else:
         print(c)  
         
 # Using Recursion
-
 def fibo(n):
     if n<=1:
         return n
@@ -29,6 +28,7 @@ else:
     print("Fibonacci series: ")
     for i in range(num):
         print(fibo(i))
+
 
 
 
