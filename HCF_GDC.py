d8c6667c0f43914db633c2fde47464e7d67b6fc1
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-27 18:02:37 +0530

    Update HCF_GDC.py

--- HCF_GDC.py
+++ HCF_GDC.py
@@ -3,6 +3,7 @@ def findHCF(x,y):
         smaller=y
     else:
         smaller=x
+        
     for i in range(1,smaller+1):
         if x % i == 0 and y % i == 0:
             hcf=i
@@ -12,6 +13,7 @@ a=int(input("Enter 1st number: "))
 b=int(input("Enter 2nd number: "))
 
 print("Hcf of given two numbers: ", findHCF(a,b))
+
 
 
 
